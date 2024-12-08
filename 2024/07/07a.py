8caffe341192e37943beaccc8b9021ec5fb37676
Simo Levanto <simo.levanto@iki.fi>
2024-12-08 21:52:59 +0200

    Fixed day 7, the input was previously handled as a dict so repeated calibration values made the code fail.

--- 2024/07/07a.py
+++ 2024/07/07a.py
@@ -5,17 +5,13 @@ file_path = "07.input"
 with open(file_path, "r", encoding="utf-8") as f:
     lines = f.readlines()
 
-inputs = {
-    int(line.split(":")[0].strip()): 
-        [int(x) for x in line.split(":")[1].strip().split(" ")] 
-        for line in lines
-        }
-
 
 def calculate(op_arr, arr, max_value):
-    res = arr.pop(0)
+    res = arr[0]
+    i = 1
+
     while len(op_arr) > 0:
-        a = arr.pop(0)
+        a = arr[i]
         op = op_arr.pop(0)
         if op == "+":
             res += a
@@ -23,17 +19,23 @@ def calculate(op_arr, arr, max_value):
             res *= a
         if res > max_value:
             return False
+        i += 1
 
     return res == max_value
 
 
-def check_calibration(end_result, equation):
+def check_calibration(line):
+    end_result_string, equation_string = line.split(":")
+
+    equation = [int(x) for x in equation_string.strip().split(" ")]
+    end_result = int(end_result_string)
+
     operators = ["+", "*"]
     op_arr = list(itertools.product(operators, repeat=len(equation)-1))
 
     for ops in op_arr:
-        if calculate(list(ops), equation.copy(), end_result):
+        if calculate(list(ops), equation, end_result):
             return end_result
     return 0
 
-print(sum(check_calibration(item, inputs[item]) for item in inputs))
+print(sum(check_calibration(line) for line in lines))
