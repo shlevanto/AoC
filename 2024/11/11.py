0546e4719a2c3fcc155f4d9f4d2860685e090847
Levanto Simo <simo.levanto@ilmarinen.fi>
2024-12-11 15:16:02 +0200

    Day 11 wip

---
+++ 2024/11/11.py
@@ -0,0 +1,7 @@
+# 1. If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+# 2. If the stone is engraved with a number that has an even number of digits, 
+# it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+# 3. If none of the other rules apply, the stone is replaced by a new stone; 
+# the old stone's number multiplied by 2024 is engraved on the new stone.
+
+# Two options -> input into new list or use linked list
