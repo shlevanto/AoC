d29e7f7ed37cea013b471e1e121257391df52880
Simo Levanto <simo.levanto@iki.fi>
2024-12-12 07:03:18 +0200

    puzzle 2 wip

--- 2024/11/11.py
+++ 2024/11/11.py
@@ -6,13 +6,16 @@
 
 # Two options -> input into new list or use linked list
 
-file_path = "11.input"
+#
+file_path = "11_short.input"
 
 with open(file_path, "r") as f:
     arr = [int(s) for s in f.read().strip().split(" ")]
 
 
 def blink(n, stones, blinked=[]):
+    
+    
     if n == 0:
         print(len(stones))
         return
@@ -28,11 +31,80 @@ def blink(n, stones, blinked=[]):
             blinked.append(a)
             blinked.append(b)
             continue
-            
+
         else:
             blinked.append(2024 * stone)
-    
+
     blink(n-1, blinked, [])
 
-print("Initial:", arr)
-blink(75, arr)
+class Counter:
+    def __init__(self):
+        self.dic = {}
+    
+    def add(self, number):
+        if number in self.dic.keys():
+            self.dic[number] = self.dic[number] + 1
+        else:
+            self.dic[number] = 1
+
+    def remove(self, number):
+        if number in self.dic.keys():
+            self.dic[number] = self.dic[number] - 1
+            if self.dic[number] < 0:
+                self.dic[number] = 0
+    
+    @property
+    def values(self):  
+         return list(self.dic.values())
+
+    @property
+    def keys(self):  
+         return list(self.dic.keys()) 
+    
+    def non_zero(self):
+        return list([a for a in self.keys() if self.dic[a] > 0])
+
+    def __str__(self):
+        return str(self.dic)
+
+
+def memo_blink(n, a_counter):
+    #print(a_counter)
+    if n == 0:
+        print(sum(a_counter.values))
+  
+        return
+
+
+    for stone in a_counter.keys:
+        if stone == 0:
+            a_counter.add(1)
+
+        if len(str(stone)) % 2 == 0 and len(str(stone)) > 1:
+            cutoff = len(str(stone)) // 2
+            a = int(str(stone)[:cutoff])
+            b = int(str(stone)[cutoff:])
+            a_counter.add(a)
+            a_counter.add(b)
+
+        else:
+            a_counter.add(2024 * stone)
+            
+        a_counter.remove(stone)
+        
+
+    memo_blink(n-1, a_counter)
+
+
+print("puzzle 1:")
+#blink(25, arr)
+
+print("****")
+
+counter = Counter()
+
+for s in arr:
+    counter.add(s)
+
+
+memo_blink(6, counter)
