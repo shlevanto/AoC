a3c45d2d6353acfc325850b82502e6806c180aed
Levanto Simo <simo.levanto@ilmarinen.fi>
2025-12-03 17:37:55 +0200

    cleanup for day 3 part 2

--- 2025/03/03b.py
+++ 2025/03/03b.py
@@ -17,10 +17,10 @@ def find_number(battery_list, number_length, result=[]):
         return result
 
     digit = max(battery_list[:list_length - number_length + 1])
-    digit_i = battery_list.index(digit) + 1
+    digit_i = battery_list.index(digit)
     result.append(digit)
 
-    return find_number(battery_list[digit_i:], number_length - 1, result)
+    return find_number(battery_list[digit_i + 1:], number_length - 1, result)
 
 
 total_output_joltage = 0
@@ -30,7 +30,7 @@ for bank in power_banks:
     joltages = list(map(int, list(bank.strip())))
     length = len(joltages)
 
-    total_output_joltage_list = find_digit(joltages, 12, [])
+    total_output_joltage_list = find_number(joltages, 12, [])
     joltage_int = int("".join(map(str, total_output_joltage_list)))
 
     total_output_joltage += joltage_int
