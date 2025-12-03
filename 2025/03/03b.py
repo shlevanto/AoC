04565bb68f5f9e4cf6fc30a24b44bf302e406109
Levanto Simo <simo.levanto@ilmarinen.fi>
2025-12-03 17:33:52 +0200

    Day 3 part 2 done using recursion.

---
+++ 2025/03/03b.py
@@ -0,0 +1,39 @@
+import sys
+
+in_file = sys.argv[1]
+
+
+with open(in_file, "r") as f:
+    power_banks = f.readlines()
+
+
+def find_number(battery_list, number_length, result=[]):
+    """Finds the largest number that can be formed by 'turning on' the specified
+    amount of digits in a given number.
+    """
+    list_length = len(battery_list)
+
+    if number_length == 0:
+        return result
+
+    digit = max(battery_list[:list_length - number_length + 1])
+    digit_i = battery_list.index(digit) + 1
+    result.append(digit)
+
+    return find_number(battery_list[digit_i:], number_length - 1, result)
+
+
+total_output_joltage = 0
+
+
+for bank in power_banks:
+    joltages = list(map(int, list(bank.strip())))
+    length = len(joltages)
+
+    total_output_joltage_list = find_digit(joltages, 12, [])
+    joltage_int = int("".join(map(str, total_output_joltage_list)))
+
+    total_output_joltage += joltage_int
+
+
+print(f"Output: {total_output_joltage}")
